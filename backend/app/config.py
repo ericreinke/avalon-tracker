ea970cb35ebedd8ea4c944192512b8c3e083ba20
Eric Reinke <ericcreinke@gmail.com>
2026-06-25 18:22:34 -0400

    fix: Fix default password

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,4 +4,4 @@ from dotenv import load_dotenv
 load_dotenv(os.path.join(os.path.dirname(__file__), "..", "..", ".env"))
 
 DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://avalon:avalon@localhost:5432/avalon")
-PASSCODE: str = os.getenv("PASSCODE", "1234")
+PASSCODE: str = os.environ["PASSCODE"]
